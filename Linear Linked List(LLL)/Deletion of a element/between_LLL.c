1ebc6593988c1eca19bf0437570896d233fca528
aayushker <singhaayushker@gmail.com>
2023-09-29 15:14:10 +0530

    Completed LLL

--- Linear Linked List(LLL)/Deletion of a element/between_LLL.c
+++ Linear Linked List(LLL)/Deletion of a element/between_LLL.c
@@ -7,13 +7,17 @@ struct node{
 };
 
 int main(){
-    struct node n1, n2,n3, *start, *ptr, *ptr1;
+    struct node n1, n2,n3,n4,n5, *start, *ptr, *ptr1;
     n1.data = 100;
     n2.data = 200;
     n3.data = 300;
+    n4.data = 400;
+    n5.data = 500;
     n1.link = &n2;
     n2.link = &n3;
-    n3.link = NULL;
+    n3.link = &n4;
+    n4.link = &n5;
+    n5.link = NULL;
     start = &n1;
     ptr = start;
 
@@ -24,14 +28,12 @@ int main(){
         int key;
         printf("Enter the element after which you want to delete");
         scanf("%d",&key);
-        while ((ptr->data==key) && (ptr != NULL)){
+        while (ptr != NULL && (ptr->data != key)){
             ptr1=ptr;
             ptr=ptr->link;
         }
-        ptr1->link=NULL;
-        free(ptr);
 
-        if (ptr== NULL){
+        if (ptr == NULL){
             printf("Key not present, exiting...");
         }
         else{
---
+++ Linear Linked List(LLL)/Deletion of a element/ending.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node{
+    int data;
+    struct node *link;
+};
+
+int main(){
+    struct node n1, n2, n3, n4, n5, *start, *New, *ptr, *ptr1;
+    New = malloc(sizeof(struct node));
+    n1.data = 100;
+    n2.data = 200;
+    n3.data = 300;
+    n4.data = 400;
+    n5.data = 500;
+    n1.link = &n2;
+    n2.link = &n3;
+    n3.link = &n4;
+    n4.link = &n5;
+    n5.link = NULL;
+    start = &n1;
+    ptr = start;
+
+    if (start == NULL){
+        printf("Underflow, exiting...");
+    }
+    else{
+        while (ptr->link != NULL){
+            ptr1=ptr;
+            ptr = ptr->link;
+        }
+        ptr1->link = NULL;
+        free(ptr);
+    }
+
+    ptr = start;
+
+    while (ptr != NULL){
+        printf("%d\n", ptr->data);
+        ptr = ptr->link;
+    }
+}
