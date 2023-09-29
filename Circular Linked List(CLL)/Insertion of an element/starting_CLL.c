c225d22b6900dd4f84636fd07d7f2c752c29b153
aayushker <singhaayushker@gmail.com>
2023-09-29 16:43:24 +0530

    Did insertion of a new node at the beginning

--- Circular Linked List(CLL)/Insertion of an element/starting_CLL.c
+++ Circular Linked List(CLL)/Insertion of an element/starting_CLL.c
@@ -3,12 +3,11 @@
 
 struct node{
     int data;
-    struct node *link;
+    struct node *link;    
 };
 
-
 int main(){
-    struct node n1, n2, n3, *new, *ptr, *last, *start;
+    struct node n1, n2, n3, *ptr, *new, *last;
     n1.data = 200;
     n2.data = 300;
     n3.data = 400;
@@ -16,27 +15,23 @@ int main(){
     n2.link = &n3;
     n3.link = &n1;
     last = &n3;
-    start= &n1;
 
     new = malloc(sizeof(struct node));
 
-    if (last == NULL){
-        new->data=100;
-        new->link=new;
-        last=new;
-    }
-    else{
-        new->data=100;
-        new->link = last->link;
-        last->link = new;
-        last = new;
+    if (new == NULL){
+        printf("Overflow, exiting...");
     }
 
-    ptr= start;
+    int ele;
+    printf("Enter the element you want to enter as first node");
+    scanf("%d",&ele);
+    new->link = last->link;
+    last->link = new;
+    new->data = ele;
 
-    do {
-        printf("%d\n", ptr->data);
-        ptr = ptr->link;
-    } while (ptr != start);
-
+    ptr = last->link;
+    do{
+        printf("%d\n",ptr->data);
+        ptr=ptr->link;
+    }while(ptr != last->link);
 }
---
+++ Circular Linked List(CLL)/traversing_CLL.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node{
+    int data;
+    struct node *link;
+};
+
+
+int main(){
+    struct node n1, n2, n3, *new, *ptr, *last;
+    
+    n1.data = 200;
+    n2.data = 300;
+    n3.data = 400;
+    n1.link = &n2;
+    n2.link = &n3;
+    n3.link = &n1;
+    last = &n3;
+
+    new = malloc(sizeof(struct node));
+
+    if (last == NULL){                                                              //For entering as a first node
+        int ele;
+        printf("Enter the element you want to enter as a first node");
+        scanf("%d", &ele);
+        new->data=ele;
+        new->link=new;
+        last=new;
+    }
+    else{                                                                           //For entering a new node
+        int ele;
+        printf("Enter the element you want to enter as a new node");
+        scanf("%d", &ele);
+        new->data=ele;
+        new->link = last->link;
+        last->link = new;
+        last = new;
+    }
+
+    ptr= last->link;
+
+    do {
+        printf("%d\n", ptr->data);
+        ptr = ptr->link;
+    } while (ptr != last->link);
+
+}
