d5fa39618ebeb20dca3ee8d1c7458b0a5df7ff0e
aayushker <singhaayushker@gmail.com>
2023-10-01 17:00:23 +0530

    Did deletion of last node in DLL

---
+++ Doubly Linked List(DLL)/Deletion of a element/ending_DLL.c
@@ -0,0 +1,41 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+struct node{
+    int data;
+    struct node *llink;
+    struct node *rlink;
+};
+
+int main(){
+    struct node n1, n2, n3, *ptr, *start;
+    n1.data = 100;
+    n2.data = 200;
+    n3.data = 300;
+    n1.llink = NULL;
+    n1.rlink = &n2;
+    n2.llink = &n1;
+    n2.rlink = &n3;
+    n3.llink = &n2;
+    n3.rlink = NULL;
+    start = &n1;
+    ptr = start;
+
+    if (start == NULL){
+        printf("Underflow, exiting...");
+    }
+    else{
+        while (ptr->rlink != NULL){
+            ptr = ptr->rlink;
+        }
+        ptr->llink->rlink = NULL;
+        ptr->llink = NULL;
+        free(ptr);
+    }
+
+    ptr = start;
+    do{
+        printf("%d\n",ptr->data);
+        ptr = ptr->rlink;
+    }while(ptr != start);
+}
