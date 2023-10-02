d1c81ba62731904a3e2f9766a092c32163940764
aayushker <singhaayushker@gmail.com>
2023-10-02 12:22:05 +0530

    Did addition of even and odd node number in LLL

---
+++ Questions related to Linked List(LL)/SumEvenOddNodeNo.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node{
+    int data;
+    struct node *link;
+};
+
+int main(){
+    struct node n1, n2, n3, n4, n5, *ptr, *start;
+    n1.data = 78; 
+    n2.data = 10; 
+    n3.data = 61; 
+    n4.data = 89; 
+    n5.data = 52; 
+    n1.link = &n2;
+    n2.link = &n3;
+    n3.link = &n4;
+    n4.link = &n5;
+    n5.link = NULL;
+    start = &n1;
+    ptr = start;
+
+    if (start == NULL){
+        printf("The given LLL is empty");
+        exit(0);
+    }
+    else{
+        int e=0,o=0,c=1;
+
+        while (ptr != NULL){
+            if ((ptr->data)%2 == 0){
+                e+=c;
+            }
+            else if((ptr->data)%2 != 0){
+                o+=c;
+            }
+            c++;
+            ptr = ptr->link;
+        }
+        printf("The sum of even node number is %d and that of odd is %d",e,o);
+    }
+    return 0;
+}
